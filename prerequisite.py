a824381f86cfd9089e231dc95ffef7e246c68303
Xiang Li <xlees58@gmail.com>
2014-10-16 22:57:39 +0800

    将utils目录添加到sys.path里。

---
+++ prerequisite.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+import sys,os
+
+filename = os.path.abspath(__file__)
+dirname = os.path.dirname(filename)
+utils = os.path.join(dirname,'utils')
+sys.path.insert(0,utils)
+#print sys.path
